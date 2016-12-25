e102be894f3a143ad3ac8c28e65af66d3adec2cc
C. Alan Zoppa <alan.zoppa@gmail.com>
2016-12-25 00:41:35 -0600

    loads integerized scores

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,13 @@ class Student
   def initialize(name, **kwargs)
     @name = name
     @scores = CommonCore::IndifferentHash.new
-    #kwargs.each do |key, value|
-      #@scores[key] 
-    #end
+    kwargs.each do |key, value|
+      @scores[key] = value.to_i
+    end
   end
+
+  def [](key)
+    @scores[key]
+  end
+
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -9,8 +9,9 @@ describe CommonCore::Student do
 
   it "unpacks and stores scores" do
     student = CommonCore::Student.new(
-      "John McGuirk",
-      RF:"2", RL:"3", RI:'K', L:"3"
+      "John McGuirk", RF:"2", RL:"3", RI:'K', L:"3"
     )
+    expect( student[:RF] ).to eql 2
+    expect( student[:RI] ).to eql 0
   end
 end
